6860abd02470b0de6cb0552c2c2f01c26b18985b
syr <shiyaorui@foxmail.com>
2021-05-02 16:17:33 +0800

    notes v1.0

--- praw/main.py
+++ praw/main.py
@@ -1,3 +1,9 @@
 import praw
+import os
+os.environ['HTTP_PROXY'] = os.environ['HTTPS_PROXY'] = 'http://localhost:12333'
 
 reddit = praw.Reddit("bot1", config_interpolation="basic")
+
+hot_posts = reddit.subreddit('wallstreetbets').hot(limit=10)
+for post in hot_posts:
+    print(post.title, end='\n\n')
