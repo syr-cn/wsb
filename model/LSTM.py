66e1deb51b4e25130ef2e9fa6e5aff9aa9042bc3
syr <shiyaorui@foxmail.com>
2021-09-14 23:02:26 +0800

    divergent model

--- model/LSTM.py
+++ model/LSTM.py
@@ -1,30 +1,38 @@
 import torch
 import torch.nn.functional as F
 
-torch.manual_seed(1)
-# random seed
-
 
 class LSTM(torch.nn.Module):
-    def __init__(self, vocab_size, input_dim, hidden_dim, output_dim):
+    def __init__(self, vocab_size, input_dim, hidden_dim, num_layers, output_dim, device, dropout=0):
         # hidden_dim aka embedding_dim
         super(LSTM, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
         self.output_dim = output_dim
+
         self.embedding = torch.nn.Embedding(vocab_size, input_dim, sparse=True)
-        self.lstm = torch.nn.LSTM(input_dim, hidden_dim)
+        # self.hidden = torch.randn(2, num_layers, 1, hidden_dim, device=device)
+        self.lstm = torch.nn.LSTM(
+            input_dim, hidden_dim, num_layers, dropout=dropout)
         self.fc = torch.nn.Linear(hidden_dim, output_dim)
         self.init_weights(0.5)
 
-    def init_weight(self, initrange):
+    def init_weights(self, initrange):
         self.embedding.weight.data.uniform_(-initrange, initrange)
+        # lstm自带正态初始化
         self.fc.weight.data.uniform_(-initrange, initrange)
-        self.fc.bias.data.zero_(-initrange, initrange)
+        self.fc.bias.data.zero_()
 
     def forward(self, text):
         embeds = self.embedding(text)
-        hidden, _ = self.lstm(embeds.view(len(text), 1, -1))
-        out_vec = self.fc(hidden.view(len(text), -1))
-        output = F.log_softmax(out_vec, dim=1)
+        embeds = embeds.view(embeds.shape[0], 1, -1)
+        # output, hidden = self.lstm(embeds, self.hidden)
+        output, _ = self.lstm(embeds)
+        output = output[-1]
+        # print(lstm_out.shape)
+        output = output.view(len(output), -1)
+        output = self.fc(output)
+        # output = F.log_softmax(output, dim=1)
+        # 激活函数可改
         return output
--- model/extractor.py
+++ model/extractor.py
@@ -7,11 +7,11 @@ import torch
 from torchtext.datasets import AG_NEWS
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
-from .LSTM import LSTM
+import LSTM
 
 tokenizer = get_tokenizer('basic_english')
 train_iter = AG_NEWS(split='train')
-train_iter = list(train_iter)
+train_iter = list(train_iter)[:20]
 # train_iter可以直接在这里改，下面yield_tokens函数也要改
 # tokenizer(str)可以把字符串分割为列表，全部转化为小写，遇到符号会单独分成一个字符（很合理），不能智能识别"'s"为"is"
 
@@ -26,14 +26,15 @@ vocab.set_default_index(vocab['<unk>'])  # 预设生词为0
 # vocab实现词与编号的映射
 
 
-def txt2idx(s):
-    return vocab(tokenizer(s))
+def txt2idx(s, device):
+    return torch.tensor(vocab(tokenizer(s)), device=device, dtype=torch.int32)
 
 
-def tag2vec(i):
+def tag2vec(i, device):
     vec = [0, 0, 0, 0]
-    vec[i] = 1
-    vec = torch.Tensor(vec)
+    vec[i-1] = 1
+    vec = torch.tensor(vec, device=device, dtype=torch.float32)
+    vec = vec.view(1, -1)
     return vec
 
 
@@ -45,18 +46,59 @@ def tag2vec(i):
 
 
 vocab_size = len(vocab)
-input_dim = 30
-hidden_dim = 25
+input_dim = 6
+hidden_dim = 6
+num_layers = 3
 output_dim = 4
+epochs = 1
+# 好像没法batch优化
+
+torch.manual_seed(2174)
+# 设定随机种子
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = LSTM(vocab_size, input_dim, hidden_dim, output_dim).to(device)
+model = LSTM.LSTM(vocab_size, input_dim, hidden_dim,
+                  num_layers, output_dim, device=device).to(device)
 loss_function = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
 with torch.no_grad():
-    lbl, txt = train_iter[0]
+    tag, txt = train_iter[0]
+    txt = txt2idx(txt, device)
+    tag = tag2vec(tag, device)
+    print(txt.shape, tag.shape)
+    print(model(txt).shape)
 
 
 def train(dataloader):
-    pass
+    for epoch in range(1, epochs+1):
+        for(tag, txt)in train_iter:
+            # reset accumulates gradients
+            model.zero_grad()
+
+            # load train data
+            trainx = txt2idx(txt, device)
+            trainy = tag2vec(tag, device)
+
+            # forward
+            resulty = model(trainx)
+
+            # gradient backward
+            loss = loss_function(resulty, trainy)
+            loss.backward()
+            optimizer.step()
+            with torch.no_grad():
+                print('-'*100)
+                print(resulty)
+                print(trainy)
+        print(f'epoch {epoch}:loss={loss}')
+
+
+train(train_iter)
+
+with torch.no_grad():
+    tag, txt = train_iter[0]
+    txt = txt2idx(txt, device)
+    tag = tag2vec(tag, device)
+    print(txt, tag)
+    print(model(txt))
